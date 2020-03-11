a0c4d07ea53dbfe06f9cdc03fae3f1f79e323a3f
Amos Welike <amoswelike@gmail.com>
2020-03-11 14:19:22 +0300

    add page design

--- src/components/fixtures/table.js
+++ src/components/fixtures/table.js
@@ -1,45 +1,45 @@
 const data_collspan = {
-    columns: [
-      {
-        label: "#",
-        field: "id",
-        sort: "asc"
-      },
-      {
-        label: "First",
-        field: "first",
-        sort: "asc"
-      },
-      {
-        label: "Last",
-        field: "last",
-        sort: "asc"
-      },
-      {
-        label: "Handle",
-        field: "handle",
-        sort: "asc"
-      }
-    ],
-    rows: [
-      {
-        id: 1,
-        first: "Mark",
-        last: "Otto",
-        handle: "@mdo"
-      },
-      {
-        id: 2,
-        first: "Jacob",
-        last: "Thornton",
-        handle: "@fat"
-      },
-      {
-        id: 3,
-        first: "Larry the Bird",
-        colspan: 2,
-        handle: "@twitter"
-      }
-    ]
-  };
-  export default data_collspan;
+  columns: [
+    {
+      field: "location",
+      sort: "asc",
+      label: "Location"
+    },
+    {
+      label: "Basic Info",
+      field: "first",
+      sort: "asc"
+    },
+    {
+      label: "Subscription Date",
+      field: "last",
+      sort: "asc"
+    },
+    {
+      label: "Status",
+      field: "handle",
+      sort: "status"
+    }
+  ],
+  rows: [
+    {
+      first: "Mark",
+      last: "Otto",
+      handle: "@mdo",
+      status: "confirmed"
+    },
+    {
+      first: "Jacob",
+      last: "Thornton",
+      handle: "@fat",
+      status: "confirmed"
+    },
+    {
+      first: "Larry the Bird",
+      last: "Thornton",
+      handle: "@twitter",
+      status: "confirmed" 
+    }
+  ]
+};
+export default data_collspan;
