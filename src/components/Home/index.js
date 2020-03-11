df152ca9b91e8071ec1f2ac577a63c12428ecb37
Amos Welike <amoswelike@gmail.com>
2020-03-11 09:55:22 +0300

    add navbar

---
+++ src/components/Home/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export class Home extends Component {
+    state = {
+    };
+    render() {
+        return (
+            <div>
+                Welcome home
+            </div>
+
+        )
+    }
+}
+
+export default Home;
